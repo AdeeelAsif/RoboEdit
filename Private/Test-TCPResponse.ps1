f46a42170bcfa2bed960ba3f5e71932301ac814d
Adeel ASIF <adeel.asif-ext@fnacdarty.com>
2020-04-03 21:38:09 +0200

    Fix for Powershell V2 on Test-TCPResponse

--- Private/Test-TCPResponse.ps1
+++ Private/Test-TCPResponse.ps1
@@ -13,66 +13,92 @@ Function Test-TCPResponse {
         [int[]]$Port,
 
         [Parameter(Mandatory = $false)]
-        [int]$TCPTimeout = 1000
+        [int]$TCPTimeout = 1000,
+
+        [Parameter(Mandatory = $false)]
+        [int]$PsVersion 
 
     )
     Process {
-        ForEach ($Computer in $ComputerName) {
 
-            ForEach ($_port in $Port) {
 
-                $stringBuilder = New-Object Text.StringBuilder
-                $tcpClient = New-Object System.Net.Sockets.TCPClient
-                $Connect = $tcpClient.ConnectAsync($Computer, $_port)
+        If ($PsVersion -le 3) {
 
-                Try {
+            $TcpClient = New-Object Net.Sockets.TcpClient
+            try {
 
-                    $null = $Connect.GetAwaiter().GetResult()
-                    $IsOpen = $True
-                }
-                Catch {
-                    
-                    $IsOpen = $False
-                }
+                $tcpClient.Connect("$ComputerName", $Port)
+                $true
+            }
+            catch {
 
-                $Object = [PSCustomObject] @{
-                    ComputerName = $Computer
-                    Port         = $_Port
-                    IsOpen       = $IsOpen
-                    Response     = $Null
-                }
+                $false
+            }
+            finally {
 
-                If ($IsOpen -eq $True) {
-                    While ($True) {
-
-                        Start-Sleep -Milliseconds 1000
-                        Write-Verbose "Bytes available: $($tcpClient.Available)"
-                        If ([int64]$tcpClient.Available -gt 0) {
-                            $Stream = $TcpClient.GetStream()
-                            $bindResponseBuffer = New-Object Byte[] -ArgumentList $tcpClient.Available
-                            [Int]$Response = $Stream.Read($bindResponseBuffer, 0, $bindResponseBuffer.count)
-                            $Null = $stringBuilder.Append(($bindResponseBuffer | ForEach-Object { [char][int]$_ }) -join '')
-                        } 
-                        Else { 
-
-                            Break 
-                        }
-                    } 
-
-                    $Object.Response = $stringBuilder.Tostring()
-
-                    If ($Stream) {
-                        $Stream.Close()
-                        $Stream.Dispose()
-                    }
-                }
-
-                $tcpClient.Close()
                 $tcpClient.Dispose()
+            }
 
-                $Object.PSTypeNames.Insert(0, 'Net.TCPResponse')
-                Write-Output $Object
-            }  
         }
+        else {
+
+            ForEach ($Computer in $ComputerName) {
+
+                ForEach ($_port in $Port) {
+
+                    $stringBuilder = New-Object Text.StringBuilder
+                    $tcpClient = New-Object System.Net.Sockets.TCPClient
+                    $Connect = $tcpClient.ConnectAsync($Computer, $_port)
+
+                    Try {
+
+                        $null = $Connect.GetAwaiter().GetResult()
+                        $IsOpen = $True
+                    }
+                    Catch {
+                    
+                        $IsOpen = $False
+                    }
+
+                    $Object = [PSCustomObject] @{
+                        ComputerName = $Computer
+                        Port         = $_Port
+                        IsOpen       = $IsOpen
+                        Response     = $Null
+                    }
+
+                    If ($IsOpen -eq $True) {
+                        While ($True) {
+
+                            Start-Sleep -Milliseconds 1000
+                            Write-Verbose "Bytes available: $($tcpClient.Available)"
+                            If ([int64]$tcpClient.Available -gt 0) {
+                                $Stream = $TcpClient.GetStream()
+                                $bindResponseBuffer = New-Object Byte[] -ArgumentList $tcpClient.Available
+                                [Int]$Response = $Stream.Read($bindResponseBuffer, 0, $bindResponseBuffer.count)
+                                $Null = $stringBuilder.Append(($bindResponseBuffer | ForEach-Object { [char][int]$_ }) -join '')
+                            } 
+                            Else { 
+
+                                Break 
+                            }
+                        } 
+
+                        $Object.Response = $stringBuilder.Tostring()
+
+                        If ($Stream) {
+                            $Stream.Close()
+                            $Stream.Dispose()
+                        }
+                    }
+
+                    $tcpClient.Close()
+                    $tcpClient.Dispose()
+
+                    $Object.PSTypeNames.Insert(0, 'Net.TCPResponse')
+                    Write-Output $Object
+                }  
+            }
+        }   
     }
 }
